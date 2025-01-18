2450360ad0c9b1b9683b9058a65a9e55d8de2218
Abdullah Al Amaan <126609300+AbdullahAlAmaan@users.noreply.github.com>
2025-01-17 20:31:09 -0500

    Reverse Bit

--- ReverseBit.cpp
+++ ReverseBit.cpp
@@ -4,7 +4,7 @@ public:
         int sum=0;
         for (int i=0;i<32;i++){
             int bit=(n>>i)&1;
-            sum |= bit << (31-i);
+            sum |= bit << (31-i); // this is to change the bit to the correct position
         }
         return sum;
     }
